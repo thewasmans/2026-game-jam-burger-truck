a6ad53cdc1231f3d14c8d81e1c502b9b34ba0c25
thewasmans <thewasmans@gmail.com>
2026-03-31 17:11:16 +0200

    Update spawn system

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,7 +5,7 @@ class_name Enemy
 @export var speed: float = 5.0
 @export var target_position: Vector3 = Vector3.ZERO
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var direction: Vector3 = (target_position - global_transform.origin).normalized()
 
 	if global_transform.origin.distance_to(target_position) > 1:
--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -4,12 +4,15 @@ class_name EnemySpawner
 
 @export var enemy_scene: PackedScene
 @export var spawn_area: BoxShape3D
+@export var target_position_clients: Node3D
+@export var target_position_mesh_debug: MeshInstance3D 
 
 var timer: Timer
 
 func _ready() -> void:
+	target_position_mesh_debug.hide()
 	timer = Timer.new()
-	timer.wait_time = 1.0
+	timer.wait_time = 2.0
 	timer.timeout.connect(spawn_enemy)
 	add_child(timer)
 	timer.start()
@@ -18,10 +21,11 @@ func spawn_enemy() -> void:
 	if enemy_scene == null:
 		return
 
-	var enemy: Node3D = enemy_scene.instantiate()
+	var enemy: Enemy = enemy_scene.instantiate()
+	enemy.target_position = target_position_clients.global_position
 	var spawn_position: Vector3 = get_random_spawn_position()
-	enemy.global_transform.origin = spawn_position
 	get_parent().add_child(enemy)
+	enemy.global_transform.origin = spawn_position
 
 func get_random_spawn_position() -> Vector3:
 	var spawn_area_size: Vector3 = spawn_area.size
