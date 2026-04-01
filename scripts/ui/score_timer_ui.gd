991def5ea64073b3d457aec8b93510b7fdcdeb88
YuniverseDev <genglercaroline@gmail.com>
2026-04-01 15:59:49 +0200

    put score timer into game ui, changed score timer script accordingly

--- scripts/ui/score_timer_ui.gd
+++ scripts/ui/score_timer_ui.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var _timer_label = $Panel/Label
+@onready var _timer_label = $/root/Main/GameUI/Panel/Label
 var _timer: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
