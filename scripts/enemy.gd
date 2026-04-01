6c339a4f89dce4852ab94e091fa39f6e6ce9b64c
thewasmans <thewasmans@gmail.com>
2026-04-01 14:59:36 +0200

    Add client ui request space world

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,7 +2,10 @@ extends CharacterBody3D
 
 class_name Enemy
 
-signal leaving_truck
+signal leaving_hungry
+signal leaving_satiated
+signal waiting_food
+signal leaved
 
 enum State { MOVING_TO_TRUCK, WAITING, LEAVING }
 
@@ -14,15 +17,22 @@ enum State { MOVING_TO_TRUCK, WAITING, LEAVING }
 var current_state: State = State.MOVING_TO_TRUCK
 var wait_timer: Timer
 
+var _is_hungry: bool
+var _is_leaved: bool
+var value_waiting: float
+
 func _ready() -> void:
+	_is_hungry = true
 	wait_timer = Timer.new()
 	wait_timer.wait_time = 3.0
 	wait_timer.one_shot = true
 	wait_timer.timeout.connect(_on_wait_timer_timeout)
 	add_child(wait_timer)
-	client_request_ui.set_request(str(randf()))
+	client_request_ui.set_request("FOOD")
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	if _is_leaved:
+		return 
 	match current_state:
 		State.MOVING_TO_TRUCK:
 			var direction: Vector3 = (target_position - global_transform.origin).normalized()
@@ -35,17 +45,34 @@ func _physics_process(_delta: float) -> void:
 				wait_timer.start()
 		
 		State.WAITING:
-			pass
+			if value_waiting >= 0:
+				value_waiting += delta
+				client_request_ui.set_waiting(wait_timer.wait_time - value_waiting)
+			else:
+				waiting_food.emit()
 			
 		State.LEAVING:
 			var direction: Vector3 = (target_leaving - global_transform.origin).normalized()
 			if global_transform.origin.distance_to(target_leaving) > 1:
 				velocity = direction * speed
 			else:
-				queue_free()
-
+				_is_leaved = true
+				hide()
+				leaved.emit()
 	move_and_slide()
 
 func _on_wait_timer_timeout() -> void:
 	current_state = State.LEAVING
-	leaving_truck.emit()
+	if _is_hungry:
+		leaving_hungry.emit()
+	else:
+		leaving_satiated.emit()
+	
+func give_food()-> bool:
+	if current_state == State.WAITING:
+		_is_hungry = false
+		wait_timer.stop()
+		current_state = State.LEAVING
+		return true
+	return false
+	
--- scripts/ui/game_ui.gd
+++ scripts/ui/game_ui.gd
@@ -3,6 +3,7 @@ extends Control
 class_name GameUI
 
 @export var reputation_slider: HSlider
+@export var button:Button
 
 func on_reputation_changed(new_reputation: int) -> void:
 	reputation_slider.value = new_reputation
