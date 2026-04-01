e961f5a3116d84bf11cd35feaabc620a5bbf2314
thewasmans <thewasmans@gmail.com>
2026-04-01 15:31:54 +0200

    Rename var button create burger

--- scripts/main.gd
+++ scripts/main.gd
@@ -9,7 +9,7 @@ var _clients:Array[Enemy] = []
 
 func _ready() -> void:
 	snack_truck.reputation_changed.connect(game_ui.on_reputation_changed)
-	game_ui.button.pressed.connect(create_burger)
+	game_ui.button_create_burger.pressed.connect(create_burger)
 
 func on_child_entered_tree(node: Node) -> void:
 	if node is Enemy:
--- scripts/ui/game_ui.gd
+++ scripts/ui/game_ui.gd
@@ -3,7 +3,7 @@ extends Control
 class_name GameUI
 
 @export var reputation_slider: HSlider
-@export var button:Button
+@export var button_create_burger:Button
 
 func on_reputation_changed(new_reputation: int) -> void:
 	reputation_slider.value = new_reputation
