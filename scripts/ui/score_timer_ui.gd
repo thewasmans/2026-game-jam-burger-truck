9d8c6e5b5e39756d5df26956c82a5f6885959ed4
YuniverseDev <genglercaroline@gmail.com>
2026-04-01 15:35:17 +0200

    Added score timer ui and script

---
+++ scripts/ui/score_timer_ui.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@onready var _timer_label = $Panel/Label
+var _timer: float = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_timer == 0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	_timer += delta
+	_timer_label.text = "Open since: " + _format_time(_timer)
+
+func _format_time(time_in_seconds: float) -> String:
+	var _total_seconds = int(time_in_seconds)
+	
+	var _hours = _total_seconds / 3600
+	var _minutes = (_total_seconds % 3600) / 60
+	var _seconds = _total_seconds % 60
+	
+	return "%02d:%02d:%02d" % [_hours,_minutes,_seconds]
