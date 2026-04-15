558ff285ea5f56b6ab44e0935ca4dfc770577310
thewasmans <thewasmans@gmail.com>
2026-04-15 19:03:15 +0200

    Add crate chopping tomato

--- scripts/crates/crate_tool.gd
+++ scripts/crates/crate_tool.gd
@@ -1,11 +1,11 @@
 class_name CrateTool
 extends CrateInterract
 
-@export var ingredient_data:IngredientData
+@export var ingredient_data:Array[IngredientData]
 var _ingredient: Ingredient
 
 func use_tool(ingredient:Ingredient):
-	if ingredient.ingredient_data != ingredient_data:
+	if not can_used_ingredient(ingredient):
 		print("CANT USE THIS TOOL WITH THE INGREDIENT")
 		return
 	var provide_ingredient := Ingredient.new()
@@ -13,3 +13,9 @@ func use_tool(ingredient:Ingredient):
 	provide_ingredient.instance = provide_ingredient.ingredient_data.model_3d.instantiate()
 	anchor_spawn.add_child(provide_ingredient.instance)
 	_ingredient = provide_ingredient
+
+func can_used_ingredient(ingredient: Ingredient):
+	for data in ingredient_data:
+		if ingredient.ingredient_data == data:
+			return true
+	return false
