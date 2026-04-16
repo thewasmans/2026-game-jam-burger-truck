6349784ca09bb9492d4676d135e7c2cafb171ea5
thewasmans <thewasmans@gmail.com>
2026-04-16 15:34:08 +0200

    Fix wrong use ingredient missing

--- scripts/crates/crate_tool.gd
+++ scripts/crates/crate_tool.gd
@@ -4,15 +4,16 @@ extends CrateInterract
 @export var ingredient_data:Array[IngredientData]
 var _ingredient: Ingredient
 
-func use_tool(ingredient:Ingredient):
+func use_tool(ingredient:Ingredient) -> bool:
 	if not can_used_ingredient(ingredient):
 		print("CANT USE THIS TOOL WITH THE INGREDIENT")
-		return
+		return false
 	var provide_ingredient := Ingredient.new()
 	provide_ingredient.ingredient_data = ingredient.ingredient_data.provide_ingredient
 	provide_ingredient.instance = provide_ingredient.ingredient_data.model_3d.instantiate()
 	anchor_spawn.add_child(provide_ingredient.instance)
 	_ingredient = provide_ingredient
+	return true
 
 func can_used_ingredient(ingredient: Ingredient):
 	for data in ingredient_data:
