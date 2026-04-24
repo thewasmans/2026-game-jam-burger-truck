5898f141e407cc330b7c3274ed8f948a75b37a0e
Nathan Mossiat <mossiat.nathan.pro@gmail.com>
2026-04-24 14:50:41 +0200

    change font size and scale size animation of the label displaying the wave

--- scripts/ui/game_ui.gd
+++ scripts/ui/game_ui.gd
@@ -66,7 +66,7 @@ func animate_label(label: Label):
 	label.pivot_offset = label.size / 2
 	label.scale = Vector2.ONE
 	label.modulate = Color.WHITE
-	tween.parallel().tween_property(label, "scale", Vector2(2.3, 2.3), 0.1)
+	tween.parallel().tween_property(label, "scale", Vector2(1.4, 1.4), 0.1)
 	tween.parallel().tween_property(label, "modulate", Color.YELLOW, 0.1)
 	tween.tween_property(label, "scale", Vector2.ONE, 0.15)
 	tween.parallel().tween_property(label, "modulate", Color.WHITE, 0.15)
