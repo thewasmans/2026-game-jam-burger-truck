d842d996f6207b90e4e316fa6386e04e8874028d
Nathan Mossiat <mossiat.nathan.pro@gmail.com>
2026-04-24 12:38:16 +0200

    add all waves data

--- scripts/data/furniture_grid_data.gd
+++ scripts/data/furniture_grid_data.gd
@@ -3,6 +3,6 @@ extends Resource
 
 @export var name: String
 @export var icon: Texture2D
-@export var model_3d: PackedScene
+@export var prefab: PackedScene
 @export var price: float
 @export var resell_price: float
