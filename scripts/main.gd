96303aaf4a73904c7cfe98aa9a103e9ceb190674
thewasmans <thewasmans@gmail.com>
2026-04-27 11:57:59 +0200

    set visisble menu furnitures

--- scripts/main.gd
+++ scripts/main.gd
@@ -25,9 +25,12 @@ func _ready() -> void:
 	game_ui.init_buttons_furnitures(furnitures)
 	game_ui.furniture_selected.connect(FurnituresManager.furniture_selected)
 	spawner.next_wave_started.connect(func(data, number):
+		game_ui.set_visible_furnitures_menu(false)
 		camera_switcher.set_camera_kitchen()
 		game_ui.set_wave_information(data, number))
-	spawner.waiting_next_wave.connect(func(): camera_switcher.set_camera_furnitures())
+	spawner.waiting_next_wave.connect(func():
+		camera_switcher.set_camera_furnitures()
+		game_ui.set_visible_furnitures_menu(true))
 	spawner.client_spawned.connect(client_spawned)
 	camera_switcher.set_camera_kitchen()
 	
