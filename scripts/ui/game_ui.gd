49a256cd5a48478b38c8359fffccffdbd66f9c4c
thewasmans <thewasmans@gmail.com>
2026-04-27 16:03:03 +0200

    Add furniutre blocks

--- scripts/ui/game_ui.gd
+++ scripts/ui/game_ui.gd
@@ -73,5 +73,4 @@ func animate_label(label: Label):
 	tween.parallel().tween_property(label, "modulate", Color.WHITE, 0.15)
 
 func set_visible_furnitures_menu(visible: bool):
-	return
 	container_menu.visible = visible
