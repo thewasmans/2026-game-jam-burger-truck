60159f2c66dfa45e9274a80bcea87b3c34c66246
YuniverseDev <genglercaroline@gmail.com>
2026-04-30 11:23:30 +0200

    prepared terrain for client oder ui integration

--- scripts/ui/client_request_ui.gd
+++ scripts/ui/client_request_ui.gd
@@ -1,17 +1,17 @@
 class_name ClientRequestUI
 extends Control
 
-@export var waiting:Slider
+@export var waiting: ProgressBar
 	
 func set_waiting(value:float):
 	waiting.value = value
 	
 	var t = value / waiting.max_value
-	var color = Color(1.0, 0.2, 0.2).lerp(Color(0.2, 1.0, 0.2, 1.0), t)
-	var style = waiting.get_theme_stylebox("grabber_area")
+	var color = Color(0.705, 0.0, 0.068, 1.0).lerp(Color(0.443, 0.858, 0.001, 1.0), t)
+	var style = waiting.get_theme_stylebox("fill")
 	if style:
 		style = style.duplicate()
-		waiting.add_theme_stylebox_override("grabber_area", style)
+		waiting.add_theme_stylebox_override("fill", style)
 		
 		if style is StyleBoxFlat:
 			style.bg_color = color
